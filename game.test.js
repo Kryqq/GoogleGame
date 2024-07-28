77c65bcc947fb134e77c39eee268673cdc90c9de
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-07-28 19:09:25 +0700

    tests for google move

--- game.test.js
+++ game.test.js
@@ -1,6 +1,16 @@
 import { Game } from './game'
 
 describe('Game Tests', () => {
+  let game
+
+  beforeEach(() => {
+    game = new Game()
+  })
+
+  afterEach(() => {
+    game.stop()
+  })
+
   it('should change status', () => {
     const game = new Game()
     game.settings = {
@@ -41,4 +51,27 @@ describe('Game Tests', () => {
       ).toBe(true)
     }
   })
+
+  it('should google change position', async () => {
+    for (let i = 0; i < 10; i++) {
+      game = new Game()
+      game.settings = {
+        gridSize: {
+          columns: 4,
+          rows: 3,
+        },
+        googleJumpInterval: 100,
+      }
+      game.start()
+      const prevGooglePositon = game.google.position.copy()
+
+      await sleep(150)
+
+      expect(prevGooglePositon.equal(game.google.position)).toBe(false)
+
+      game.stop()
+    }
+  })
+
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 })
